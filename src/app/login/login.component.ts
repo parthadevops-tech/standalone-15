7a564b46662286b5d7305497a47b4fa8563e0a93
parthadevops-tech <jc90pc@gmail.com>
2024-10-31 21:23:04 +0530

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -47,6 +47,7 @@ export class LoginComponent implements OnInit {
       )
       .subscribe((authorDetails: any) => {
         console.log(authorDetails);
+        console.log("Wel come to login repo");
       });
 
     this.service
